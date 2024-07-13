7aed501f46e5580cc26bb4cd99253b305c78b3b1
Pranshu021 <pranshu.duneja@wipro.com>
2024-07-13 14:57:01 +0530

    INIT

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+    const [data, setData] = useState("")
+
+    const fetchTestData = async() => {
+        const response = await axios.get("http://localhost:3001/api/test")
+        console.log(response.data.message)
+        setData(response.data.message)
+    }
+
+    useEffect(() => {
+        console.log("Something")
+        fetchTestData()
+    }, [])
+
+    return (
+        <div>
+            {data ? <h1>{data}</h1> : <h1>Welcome to homepage</h1>}
+        </div>
+    )
+}
+
+export default App
